a184e37358152c0a4b02b2f0388812730df1d8eb
melodicodyssey <stevedbauer@gmail.com>
2014-04-26 15:47:36 -0700

    migrated and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Widget.create(name: "gombers", quantity: 10)
+Widget.create(name: "hydrospanners", quantity: 13)
+Widget.create(name: "bulb-wrenches", quantity: 5)
+Widget.create(name: "flux capacitors", quantity: 1)
+Widget.create(name: "ion torches", quantity: 3)
+Widget.create(name: "hairpins", quantity: 56)
+Widget.create(name: "squiggle drivers", quantity: 8)
+Widget.create(name: "vine lashes", quantity: 23)
+Widget.create(name: "prickles", quantity: 41)
+Widget.create(name: "weird things", quantity: 462)
