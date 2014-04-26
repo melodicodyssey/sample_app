e8125e99c2146630ddddeef35b7fd479bae82611
melodicodyssey <stevedbauer@gmail.com>
2014-04-26 15:15:02 -0700

    created WidgetsController + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
 SampleApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root "widgets#index"
+  resources :widgets
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+# Routes:
+#        Prefix Verb   URI Pattern                 Controller#Action
+#   ----------- -----  --------------------------  -----------------
+#        root   GET    /                           widgets#index
+#     widgets   GET    /widgets(.:format)          widgets#index
+#               POST   /widgets(.:format)          widgets#create
+#  new_widget   GET    /widgets/new(.:format)      widgets#new
+# edit_widget   GET    /widgets/:id/edit(.:format) widgets#edit
+#      widget   GET    /widgets/:id(.:format)      widgets#show
+#               PATCH  /widgets/:id(.:format)      widgets#update
+#               PUT    /widgets/:id(.:format)      widgets#update
+#               DELETE /widgets/:id(.:format)      widgets#destroy
 end
